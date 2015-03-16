eea6058828d20afee0aa2f272bee25774e7b1512
vagan96@gmail.com <15031996aVk>
2015-03-16 23:43:54 +0300

    Initial commit

--- HeapSort/ProgrammaHeapSort.dpr
+++ HeapSort/ProgrammaHeapSort.dpr
@@ -5,50 +5,61 @@ program HeapSort;
 uses
 SysUtils;
 
-const
-M = 1000000;
-
-type
-mas = array [1..M] of Integer;
-
 var
-a: mas;
-n, i, j, min, k,p: Integer;
+A: array of integer;
+n, i, x, l,r: longword;
 f: TextFile;
 T: TDateTime;
-name1,name2,vopr: string[20];
+name1,name2: string[20];
 THour, TMin, TSec, TMilli: word;
 TSum: double;
+
+procedure sift(L, R: integer);
+var
+i, j: integer;
+x: integer;
+begin
+i := L;
+j := 2 * i;
+x := a[i];
+if (j < R) and (a[j] < a[j + 1]) then
+j := j + 1;
+while (j <= R) and (x < a[j]) do
+begin
+a[i] := a[j];
+i := j;
+j := 2 * j;
+if (j < R) and (a[j] < a[j + 1]) then
+j := j + 1;
+end;
+a[i] := x;
+end;
 
 begin
 name1:=paramstr(1);
 name2:=paramstr(2);
-Writeln('Vvedite YES - poshagovoy sortirovka, NO - obichnay sortirovka');
-Readln(vopr);
-Assignfile(f, name1);
-reset(f);
-readln(f, n);
-for i:= 1 to n do
+AssignFile(f, name1);
+Reset(f);
+Readln(f, n);
+setlength(a,n);
+for i:= 0 to n-1 do
 read(f, a[i]);
 Close(f);
 T:= Now;
-if (vopr = 'no') then
+L := (N div 2)+1;
+R := N;
+while L > 0 do
 begin
-for i:= 1 to n-1 do
-begin
-min:= a[i];
-k:= i;
-for j:=i+1 to n do
-if a[j]<min then
-begin
-min:= a[j];
-k:= j;
+L := L - 1;
+sift(L, R);
 end;
-if k<>i then
+while R > 0 do
 begin
-a[k]:= a[i];
-a[i]:= min;
-end;
+x := a[0];
+a[0] := a[R];
+a[R] := x;
+R := R - 1;
+sift(L, R);
 end;
 T:= Now-T;
 DecodeTime(T, THour, TMin, TSec, TMilli);
@@ -56,42 +67,10 @@ TSum:= TMilli/1000 + TSec + 60*TMin + 3600*THour;
 AssignFile(f, name2);
 rewrite(f);
 writeln(f, n);
-for i:= 1 to n-1 do
+for i:= 0 to n-1 do
 write(f, a[i], ' ');
-writeln(f, a[n]);
-Writeln(f, 'Vybor');
-Writeln(f, floattostr(TSum));
-CloseFile(f);
-end
-else
-begin
-AssignFile(f, name2);
-rewrite(f);
-writeln(f, n);
-for i:= 1 to n-1 do
-begin
-min:= a[i];
-k:= i;
-for j:=i+1 to n do
-if a[j]<min then
-begin
-min:= a[j];
-k:= j;
-end;
-if k<>i then
-begin
-a[k]:= a[i];
-a[i]:= min;
-end;
-for p:= 1 to n do
-write(f, a[p], ' ');
 writeln(f);
-end;
-T:= Now-T;
-DecodeTime(T, THour, TMin, TSec, TMilli);
-TSum:= TMilli/1000 + TSec + 60*TMin + 3600*THour;
-Writeln(f, 'Selection');
+Writeln(f, 'HeapSort');
 Writeln(f, floattostr(TSum));
 CloseFile(f);
-end;
 end.
